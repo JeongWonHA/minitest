38a76fcd1ba04029f28d8d1e4dd4ee52a66d4396
Ha jeongwon <hajeongwon77@gmail.com>
2020-04-25 19:57:36 +0900

    Add saveData fun

--- manager.c
+++ manager.c
@@ -38,9 +38,14 @@ int selectDataNo(Product *p, int count){
 void saveData(Product p[], int count){
 	FILE* fp;
 
-	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for( int i=0; i< count; i++){
+		if(p[i].weight == -1){
+			count ++;
+			continue;
+		}
+	fprintf(fp,"%s %d %d",p[i].name, p[i].weight, p[i].price);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
